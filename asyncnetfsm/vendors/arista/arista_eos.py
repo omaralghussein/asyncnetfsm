3acf5da7e8cb3e3d2596eba152e53185203fb318
Omar Al-Ghussein <z3@live.it>
2019-11-06 14:26:35 +0200

    fix arista config check

--- asyncnetfsm/vendors/arista/arista_eos.py
+++ asyncnetfsm/vendors/arista/arista_eos.py
@@ -1,8 +1,10 @@
 from asyncnetfsm.vendors.ios_like import IOSLikeDevice
+from asyncnetfsm.logger import logger
 
 
 class AristaEOS(IOSLikeDevice):
     """Class for working with Arista EOS"""
+
     def __init__(self, secret=u"", *args, **kwargs):
         """
         Initialize class for asynchronous working with network devices
@@ -40,4 +42,29 @@ class AristaEOS(IOSLikeDevice):
 
     _config_check = ")#"
     """Checking string in prompt. If it's exist im prompt - we are in configuration mode"""
+
+    async def exit_config_mode(self):
+        """Exit from configuration mode"""
+        logger.info("Host {}: Exiting from configuration mode".format(self._host))
+        output = ""
+        exit_config = type(self)._config_exit
+        if await self.check_config_mode():
+            self._stdin.write(self._normalize_cmd(exit_config))
+            output = await self._read_until_prompt()
+            output = output.replace("(s1)", "")
+            output = output.replace("(s2)", "")
+            if await self.check_config_mode():
+                raise ValueError("Failed to exit from configuration mode")
+        return output
+
+    async def check_config_mode(self):
+        """Checks if the device is in configuration mode or not"""
+        logger.info("Host {}: Checking configuration mode".format(self._host))
+        check_string = type(self)._config_check
+        self._stdin.write(self._normalize_cmd("\n"))
+        output = await self._read_until_prompt()
+        output = output.replace("(s1)", "")
+        output = output.replace("(s2)", "")
+        return check_string in output
+
     pass
