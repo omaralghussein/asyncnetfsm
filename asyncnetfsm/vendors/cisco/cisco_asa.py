6ee425731513c7ab6b5cfcf104ad59e5e3b414a0
Omar Al-Ghussein <aghussein@mobilenoc.mobi>
2020-01-19 19:29:07 +0200

    fix asa connection

--- asyncnetfsm/vendors/cisco/cisco_asa.py
+++ asyncnetfsm/vendors/cisco/cisco_asa.py
@@ -1,9 +1,7 @@
 """Subclass specific to Cisco ASA"""
 
-import re
-
-from asyncnetfsm.logger import logger
 from asyncnetfsm.vendors.ios_like import IOSLikeDevice
+from asyncnetfsm.logger import logger
 
 
 class CiscoASA(IOSLikeDevice):
@@ -36,57 +34,20 @@ class CiscoASA(IOSLikeDevice):
         """ Returning Bool True if ASA in multiple mode"""
         return self._multiple_mode
 
-    async def connect(self):
-        """
-        Async Connection method
-
-        Using 5 functions:
-
-        * _establish_connection() for connecting to device
-        * _set_base_prompt() for finding and setting device prompt
-        * _enable() for getting privilege exec mode
-        * _disable_paging() for non interact output in commands
-        *  _check_multiple_mode() for checking multiple mode in ASA
-        """
-        logger.info("Host {}: trying to connect to the device".format(self._host))
-        await self._establish_connection()
-        await self._set_base_prompt()
-        await self.enable_mode()
-        await self._disable_paging()
+    async def _session_preparation(self):
+        await super()._session_preparation()
+        # await self._disable_width()
         await self._check_multiple_mode()
-        logger.info("Host {}: Has connected to the device".format(self._host))
 
-    async def _set_base_prompt(self):
-        """
-        Setting two important vars for ASA
-            base_prompt - textual prompt in CLI (usually hostname)
-            base_pattern - regexp for finding the end of command. IT's platform specific parameter
-
-        For ASA devices base_pattern is "prompt([\/\w]+)?(\(.*?\))?[#|>]
-        """
-        logger.info("Host {}: Setting base prompt".format(self._host))
-        prompt = await self._find_prompt()
-        # Cut off prompt from "prompt/context/other" if it exists
-        # If not we get all prompt
-        prompt = prompt[:-1].split("/")
-        prompt = prompt[0]
-        self._base_prompt = prompt
-        delimiters = map(re.escape, type(self)._delimiter_list)
-        delimiters = r"|".join(delimiters)
-        base_prompt = re.escape(self._base_prompt[:12])
-        pattern = type(self)._pattern
-        self._base_pattern = pattern.format(prompt=base_prompt, delimiters=delimiters)
-        logger.debug("Host {}: Base Prompt: {}".format(self._host, self._base_prompt))
-        logger.debug("Host {}: Base Pattern: {}".format(self._host, self._base_pattern))
-        return self._base_prompt
+    async def _disable_width(self):
+        logger.info("setting terminal width to 511")
+        await self.send_config_set([type(self)._disable_width_command])
 
     async def _check_multiple_mode(self):
         """Check mode multiple. If mode is multiple we adding info about contexts"""
-        logger.info("Host {}:Checking multiple mode".format(self._host))
-        out = await self.send_command("show mode")
+        logger.info("Checking multiple mode")
+        out = await self.send_command_expect("show mode")
         if "multiple" in out:
             self._multiple_mode = True
 
-        logger.debug(
-            "Host {}: Multiple mode: {}".format(self._host, self._multiple_mode)
-        )
+        logger.debug("Multiple mode: %s" % self._multiple_mode)
