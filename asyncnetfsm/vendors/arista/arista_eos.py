1c2312817602fe1d3a4b9da4399ca61fbed54f74
Omar Al-Ghussein <aghussein@mobilenoc.mobi>
2020-01-20 18:39:04 +0200

    fix arista (/S+) prompt

--- asyncnetfsm/vendors/arista/arista_eos.py
+++ asyncnetfsm/vendors/arista/arista_eos.py
@@ -5,25 +5,8 @@ from asyncnetfsm.logger import logger
 class AristaEOS(IOSLikeDevice):
     """Class for working with Arista EOS"""
 
-    def __init__(self, secret=u"", *args, **kwargs):
-        """
-        Initialize class for asynchronous working with network devices
-
-        :param str host: device hostname or ip address for connection
-        :param str username: username for logging to device
-        :param str password: user password for logging to device
-        :param str secret: secret password for privilege mode
-        :param int port: ssh port for connection. Default is 22
-        :param str device_type: network device type
-        :param known_hosts: file with known hosts. Default is None (no policy). With () it will use default file
-        :param str local_addr: local address for binding source of tcp connection
-        :param client_keys: path for client keys. Default in None. With () it will use default file in OS
-        :param str passphrase: password for encrypted client keys
-        :param float timeout: timeout in second for getting information from channel
-        :param loop: asyncio loop object
-        """
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._secret = secret
 
     _priv_enter = "enable"
     """Command for entering to privilege exec"""
