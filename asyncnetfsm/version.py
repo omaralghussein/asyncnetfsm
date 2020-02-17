ca20c7a81e1427858899aaa4e700d95f70019dd9
Omar Al-Ghussein <aghussein@mobilenoc.mobi>
2020-02-17 17:37:01 +0200

    fixed problem for FTD

--- asyncnetfsm/version.py
+++ asyncnetfsm/version.py
@@ -1,7 +1,7 @@
 """ AsyncNetFSM Version information
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Omar Al-Ghussein"
 __author_email__ = "z3@live.it"
 __url__ = "https://o-g.me"
